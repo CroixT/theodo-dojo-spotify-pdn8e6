eeaad94ac991f68749e0c221396f155d80acaad3
CroixT <nverdeyme.nv@gmail.com>
2024-02-20 20:20:02 +0100

    v5

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,4 @@
-const apiToken = 'Bearer BQDkahcKxWm0HgzJyTorLG6v4tdUFS96ypf4H9e_P6nhG7pU0mWEOnf6Wr1lPKP-_AC5JwMSgmvajHxXISVi_ATCmk80TVRS8d8YGivcgZqiAT5BAiZpl2mx4crbRlusvhqGMpjLkomWX1wKjjJiecr5v0DSfX4b9P_pxt7VbfbtyTBJgcT2c94f7Q20-TTdG05cgDcWqLBBGHML7N27GPYP7ebHIA';
+const apiToken = 'BQDkahcKxWm0HgzJyTorLG6v4tdUFS96ypf4H9e_P6nhG7pU0mWEOnf6Wr1lPKP-_AC5JwMSgmvajHxXISVi_ATCmk80TVRS8d8YGivcgZqiAT5BAiZpl2mx4crbRlusvhqGMpjLkomWX1wKjjJiecr5v0DSfX4b9P_pxt7VbfbtyTBJgcT2c94f7Q20-TTdG05cgDcWqLBBGHML7N27GPYP7ebHIA';
 
 export const fetchTracks = async () => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
